144a9cce558448718628c5c07935906d4fcb5e8c
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-14 12:44:16 +0530

    Update start.py

--- ARYAN/plugins/bot/start.py
+++ ARYAN/plugins/bot/start.py
@@ -85,8 +85,8 @@ async def start_pm(client, message: Message, _):
             accha = await message.reply_text(
             text=random.choice(EMOJIOS),
              await asyncio.sleep(2)
-        await umm.delete()
-        await message.reply_sticker(sticker=random.choice(STICKER))
+             await umm.delete()
+             await message.reply_sticker(sticker=random.choice(STICKER))
             return await message.reply_photo(
                 random.choice(ARYAN_PICS),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
