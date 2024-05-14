0999f1d55b256f66af70a7cd1ce12f029af8f530
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-14 11:13:52 +0530

    Update start.py

--- ARYAN/plugins/bot/start.py
+++ ARYAN/plugins/bot/start.py
@@ -51,6 +51,29 @@ ARYAN_PICS = [
 "https://telegra.ph/file/1dc71c11762d4c4193128.jpg"
 ]
 
+EMOJIOS = [
+    "👋",
+    "✨",
+    "❤️",
+    "😚",
+    "😇",
+    "💫",
+    "💋",
+    "🌿",
+    "🚩",
+    "👅",
+]
+
+STICKER = [
+"CAACAgUAAxkBAAEEaoFmQvmHm8XwyuA9-r4ZQbkQfwFmrwAClAkAAuqHMVbcrC536UP9uTUE",
+"CAACAgUAAxkBAAEEaoBmQvmGhrqHVtpi6E5E9PKJ9a2_gQACugkAAo69MFae-bG4s_Gv3zUE",
+"CAACAgUAAxkBAAEEan9mQvmBkzEF-35gtjndVr011sMnnwACYQgAAuMpMVYiBOEX1pjQSDUE",
+"CAACAgUAAxkBAAEEan5mQvl_GiohVEm1HJMOkXknTzPojgACDQoAAj3zKVa_7cKVuhmQqDUE",
+"CAACAgUAAxkBAAEEan1mQvl73fx9ow1sgkAXZWov8nZYcAACaysAAqjnqFekxYD_S35p3TUE",
+"CAACAgUAAxkBAAEEanxmQvl1qAFu6lauR3A8oTYMexl2QwACFAkAAv1uEFaB9MuGNAYvsTUE",
+"CAACAgUAAxkBAAEEantmQvlv-0LuilYfTk-r4OJv_spS7gACVQ0AAq73GFajcdikVQnZozUE"
+]
+
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
