970df1f6ae02e04e88f453e58c26481887def062
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-15 16:54:07 +0530

    Add files via upload

---
+++ ARYAN/zexx.py
@@ -0,0 +1,19 @@
+STICKER = [
+    "CAACAgEAAxkBAAMoZZ1kZjw6mgLiHFrqWQABYaMgVzq6AALsAQACc-mxR4CpqRkUZVFtHgQ",
+    "CAACAgEAAxkBAAMnZZ1kVlWO8ghxbU57PwHBY2Wa57oAAswBAAJB87hHUkWvBVh2glweBA",
+    "CAACAgEAAxkBAAMmZZ1kVUeZKSWzzzoFUx38twABVHCBAAKIAQACwry5R26SU50_bxSHHgQ",
+]
+
+
+EMOJIOS = [
+    "💣",
+    "💥",
+    "🪄",
+    "🧨",
+    "⚡",
+    "🤡",
+    "👻",
+    "🎃",
+    "🤤",
+    "💞",
+]
