024a5a4b48417e56c80f3ff0b8db5ff6ce5b4a2a
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-15 16:55:18 +0530

    Update and rename zexx.py to aryan.py

---
+++ ARYAN/aryan.py
@@ -0,0 +1,24 @@
+STICKER = [
+"CAACAgUAAxkBAAEEaoFmQvmHm8XwyuA9-r4ZQbkQfwFmrwAClAkAAuqHMVbcrC536UP9uTUE",
+"CAACAgUAAxkBAAEEaoBmQvmGhrqHVtpi6E5E9PKJ9a2_gQACugkAAo69MFae-bG4s_Gv3zUE",
+"CAACAgUAAxkBAAEEan9mQvmBkzEF-35gtjndVr011sMnnwACYQgAAuMpMVYiBOEX1pjQSDUE",
+"CAACAgUAAxkBAAEEan5mQvl_GiohVEm1HJMOkXknTzPojgACDQoAAj3zKVa_7cKVuhmQqDUE",
+"CAACAgUAAxkBAAEEan1mQvl73fx9ow1sgkAXZWov8nZYcAACaysAAqjnqFekxYD_S35p3TUE",
+"CAACAgUAAxkBAAEEanxmQvl1qAFu6lauR3A8oTYMexl2QwACFAkAAv1uEFaB9MuGNAYvsTUE",
+"CAACAgUAAxkBAAEEantmQvlv-0LuilYfTk-r4OJv_spS7gACVQ0AAq73GFajcdikVQnZozUE",
+]
+    
+
+
+EMOJIOS = [
+    "💣",
+    "💥",
+    "🪄",
+    "🧨",
+    "⚡",
+    "🤡",
+    "👻",
+    "🎃",
+    "🤤",
+    "💞",
+]
