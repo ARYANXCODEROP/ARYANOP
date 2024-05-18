7446627af1bf6791c4123a65643024679714f098
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-18 10:30:57 +0530

    Update __main__.py

--- ARYAN/__main__.py
+++ ARYAN/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await ARYAN.start()
     try:
-        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await ARYAN.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("ARYAN").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
