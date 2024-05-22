53be6f61ae34fd510ca5a6762398372ac1253dab
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-22 07:03:58 +0530

    Update copyright.py

--- ARYAN/plugins/aryan/copyright.py
+++ ARYAN/plugins/aryan/copyright.py
@@ -5,9 +5,9 @@ from pyrogram.types import Message
 # -----------------------------------------------------
 
 @app.on_edited_message(filters.group & ~filters.me)
-async def delete_edited_messages(client, edited_message, message):
+async def delete_edited_messages(client, edited_message):
     delete_message = f"{message.from_user.mention} ᴍᴇssᴀɢᴇ ᴇᴅɪᴛ ᴍᴀᴛ ᴋᴀʀᴏ"
-    await message.reply_text(delete_message)
+    await edited_message.reply_text(delete_message)
     await edited_message.delete()
 
 
