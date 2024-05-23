c8cdcb5a9fefc696ba737898ca1af321d6734f51
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-23 20:46:24 +0530

    Create repo.py

---
+++ repo.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ARYAN import app
+from ARYAN.utils.errors import capture_err
+import httpx 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+start_txt = """
+ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ
+"""
+
+
+
+
+@app.on_message(filters.command("repo"))
+async def start(_, msg):
+    buttons = [
+        [ 
+          InlineKeyboardButton("𝗔𝗗𝗗 𝗠𝗘", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        ]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/faa1f3ad7116e33d9f402.jpg",
+        caption=start_txt,
+        reply_markup=reply_markup
+    )
