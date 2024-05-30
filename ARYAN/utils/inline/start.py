1e17809d0f54a546385db875fb7f9aadd7dfc222
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-30 14:48:09 +0530

    Update start.py

--- ARYAN/utils/inline/start.py
+++ ARYAN/utils/inline/start.py
@@ -25,7 +25,7 @@ def private_panel(_):
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="amhelper")],
         [
             InlineKeyboardButton(text=_["S_B_2"], callback_data="lood"),
             InlineKeyboardButton(text=_["S_B_7"], callback_data="repoxlove"),
