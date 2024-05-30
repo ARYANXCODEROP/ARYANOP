70b91051e4935b763c951652281c0a51ccfb3ae9
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-05-30 14:47:32 +0530

    Update help.py

--- ARYAN/plugins/bot/help.py
+++ ARYAN/plugins/bot/help.py
@@ -175,6 +175,17 @@ async def helper_cb(client, CallbackQuery, _):
      InputMediaPhoto(random.choice(ARYAN_PICS),
                caption=helpers.HELP_15),
         reply_markup=keyboard)
+
+
+@app.on_callback_query(filters.regex("amhelper") & ~BANNED_USERS)
+@languageCB
+async def helper(client, CallbackQuery, _):
+    aryanpage = first_page(_)
+    await CallbackQuery.edit_message_media(
+     InputMediaPhoto(random.choice(ARYAN_PICS),
+               caption=_["help_1"].format(SUPPORT_CHAT),
+            ),
+        reply_markup=aryanpage)
     
 @app.on_callback_query(filters.regex("aryan") & ~BANNED_USERS)
 @languageCB
