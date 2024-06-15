55268b4127ceb2b0d8e8f1d648a06a80a7f010dc
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-06-15 09:08:53 +0530

    Update settings.py

--- ARYAN/plugins/bot/settings.py
+++ ARYAN/plugins/bot/settings.py
@@ -66,7 +66,7 @@ async def love_repo(client, CallbackQuery, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-     InputMediaVideo("https://telegra.ph/file/fcfdbf7f22a2898d207cd.mp4", has_spoiler=True, caption="ʟᴜɴᴅ ʟᴇʟᴇ ʙʜᴏsᴅɪᴋᴇ"),
+ InputMediaVideo("https://telegra.ph/file/fcfdbf7f22a2898d207cd.mp4", has_spoiler=True, caption="ʟᴜɴᴅ ʟᴇʟᴇ ʙʜᴏsᴅɪᴋᴇ"),
         reply_markup=source1_markup(_))
 
 @app.on_callback_query(filters.regex("love") & ~BANNED_USERS)
