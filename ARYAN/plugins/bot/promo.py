9079668f4b86e2824964f5abee4cbcb3a99c2fe2
ARYANXCODEROP <sshkntgsashikant@gmail.com>
2024-06-21 19:15:23 +0530

    Create promo.py

---
+++ ARYAN/plugins/bot/promo.py
@@ -0,0 +1,35 @@
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ARYAN import app
+from ARYAN.utils.errors import capture_err
+import httpx 
+import config
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+promo_txt = """
+<b><u>💰 Dᴏɴᴀᴛᴇ ғᴏʀ ᴘᴀɪᴅ ᴘʀᴏᴍᴏᴛɪᴏɴ 💸</b></u>\nᴜᴘɪ ɪᴅ : <b><u>teammadmaxop@axl</b></u>
+"""
+
+
+
+
+@app.on_message(filters.command("promo"))
+async def start(_, msg):
+    buttons = [
+        [
+              InlineKeyboardButton(
+                        text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=config.OWNER_ID
+                    ),
+              ],
+               ]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await msg.reply_photo(
+        photo="https://telegra.ph/file/2866697eebaa53ca899f4.jpg", has_spoiler=True,
+        caption=promo_txt,
+        reply_markup=reply_markup
+    )
+ 
+   
+# --------------
